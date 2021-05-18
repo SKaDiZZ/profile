8b4490df88bbd5db7d96820cfe41b7e3308b4a20
SKaDiZZ <akirapowered@gmail.com>
2021-05-18 21:55:16 +0200

    added home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import { Route, Switch, Link } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-bg": "var(--main-bg)",
+        "main-fg": "var(--main-fg)",
+        "main-brand": "var(--main-brand)",
+      },
+    },
   },
   variants: {
     extend: {},
