b3e7018848d5a69d0ba985c86f23630fef653ad6
SKaDiZZ <akirapowered@gmail.com>
2021-05-19 16:50:27 +0200

    updated config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, NavLink } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mx-auto px-6 sm:px-0">
+      <header className="h-32 flex justify-between items-center border-b">
+        <nav>
+          <ul className="flex gap-6 text-xl font-medium">
+            <li>
+              <NavLink to="/home">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/home">About Me</NavLink>
+            </li>
+            <li>
+              <NavLink to="/home">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/home">Experience</NavLink>
+            </li>
+            <li>
+              <NavLink to="/home">Contact Me</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Switch>
         <Route path="/" exact={true}>
           <Home />
