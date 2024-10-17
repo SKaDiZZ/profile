a7939535055c41af4091707908959885d2527fbd
SKaDiZZ <akirapowered@gmail.com>
2024-10-17 18:37:55 +0200

    new astro project setup and some base styling

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'fg': '#808dad',
+				'dark': '#090c10',
+				'deep-blue': '#0e1630',
+			}
+		},
+	},
+	plugins: [],
+}
