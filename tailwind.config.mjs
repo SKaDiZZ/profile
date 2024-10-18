dbe24715485044cda3d8ac3d4a10d8e1d7fca59e
SKaDiZZ <akirapowered@gmail.com>
2024-10-18 16:43:04 +0200

    added card components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'fg': '#808dad',
-				'dark': '#090c10',
-				'deep-blue': '#0e1630',
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        fg: "#808dad",
+        dark: "#090c10",
+        "deep-blue": "#0e1630",
+        brand: "var(--main-brand)",
+        accent: "var(--main-accent)",
+      },
+    },
+  },
+  plugins: [],
+};
