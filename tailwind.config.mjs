0597ac50bbb0961fccaa04f5a8e4a2cdc0078fa2
SKaDiZZ <akirapowered@gmail.com>
2024-11-07 20:43:29 +0100

    added magic ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,81 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        fg: "#808dad",
-        dark: "#090c10",
-        "background-dark": "var(--main-bg)",
-        "deep-blue": "#0e1630",
-        brand: "var(--main-brand)",
-        accent: "var(--main-accent)",
-      },
-      fontFamily: {
-        main: ["Comfortaa", "sans-serif"],
-        heading: ["Poppins", "serif"],
-      },
-    },
+  	container: {
+  		center: 'true',
+  		padding: {
+  			DEFAULT: '1.5rem',
+  			sm: '1.5rem',
+  			lg: '1.5rem',
+  			xl: '0',
+  			'2xl': '0'
+  		},
+  		screens: {
+  			lg: '767px',
+  			xl: '1023px',
+  			'2xl': '1252px'
+  		}
+  	},
+  	extend: {
+  		colors: {
+  			fg: '#808dad',
+  			dark: '#090c10',
+  			'background-dark': 'var(--main-bg)',
+  			'deep-blue': '#0e1630',
+  			brand: 'var(--main-brand)',
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			main: ["Comfortaa", "sans-serif"],
+  			heading: ["Poppins", "serif"]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
